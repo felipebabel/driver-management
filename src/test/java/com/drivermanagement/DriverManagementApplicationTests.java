82f8b6dbf39071c345f5d9377ea127e2fcc129e3
Felipe Babel <babelfelipe@gmail.com>
2024-11-17 21:55:01 -0300

    feat: added spring

---
+++ src/test/java/com/drivermanagement/DriverManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
